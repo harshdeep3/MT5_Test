76e17a18c781defbe530228ade2e465fa3477725
harshdeep3 <harshdeep366@gmail.com>
2023-04-10 13:14:00 +0100

    updating the code to reterive historical data

--- mtLogin.py
+++ mtLogin.py
@@ -1,10 +1,12 @@
 import MetaTrader5 as mt5
 import pandas as pd
+from datetime import datetime
+import pytz
 
-LOGIN = 51104994
+LOGIN = 51176525
 SERVER = "ICMarketsSC-Demo"
 # password
-PASSWORD = "Q1VqLHUs"
+PASSWORD = "53kZPYq6"
 
 # display data on the MetaTrader 5 package
 print("MetaTrader5 package author: ", mt5.__author__)
@@ -38,14 +40,38 @@ class MT5Class:
             print("Account info is None!")
         else:
             account_info_dict = mt5.account_info()._asdict()
-            self.account_info = pd.DataFrame(list(account_info_dict.items()),columns=['property','value'])
+            self.account_info = pd.DataFrame(list(account_info_dict.items()), columns=['property', 'value'])
             print(self.account_info)
 
 
-if __name__ == "__main__":
+def get_historic_data(fx_symbol, fx_timeframe, fx_count):
 
+    rates = mt5.copy_rates_from_pos(fx_symbol, fx_timeframe, 0, fx_count)
+    # dataframe
+    historic_df = pd.DataFrame(rates)
+    print(historic_df.head())
+    historic_df['time'] = pd.to_datetime(historic_df['time'], unit='s')
+
+    return historic_df
+
+
+if __name__ == "__main__":
     mt5_obj = MT5Class()
     mt5_obj.login_to_metatrader()
     mt5_obj.get_acc_info()
+
+    # timeframe objects https://www.mql5.com/en/docs/python_metatrader5/mt5copyratesfrom_py
+    timeframe = mt5.TIMEFRAME_D1
+    symbol = 'USDJPY'
+    # set time zone to UTC
+    timezone = pytz.timezone("Etc/UTC")
+    # create 'datetime' objects in UTC time zone to avoid the implementation of a local time zone offset
+    utc_from = datetime(2010, 1, 10, tzinfo=timezone)
+    utc_to = datetime(2020, 1, 11, tzinfo=timezone)
+    count = 13500
+
+    df = get_historic_data(symbol, timeframe, count)
+
+    print(df)
     # Disconnect from the terminal
     mt5.shutdown()
