e4d1064151ee07b71c0b4ad67e885713d44c4fb9
harshdeep3 <harshdeep366@gmail.com>
2024-05-08 21:18:20 +0100

    adding comments

--- mtLogin.py
+++ mtLogin.py
@@ -27,12 +27,18 @@ class MT5Class:
         self.account_info = None
 
     def login_to_metatrader(self, login=LOGIN, password=PASSWORD) -> None:
+        """
+        Login into meta trader 5 account
+
+        Args:
+            login (_type_, optional): _description_. Defaults to LOGIN. Account login
+            password (_type_, optional): _description_. Defaults to PASSWORD. Password for account
+        """
         # Connect to the MetaTrader 5 terminal
         mt5.initialize()
 
         # Log in to the terminal with your account credentials
         account_server = SERVER
-
         self.mt5_result = mt5.login(login, password, account_server)
 
         if not self.mt5_result:
@@ -40,7 +46,9 @@ class MT5Class:
             quit()
 
     def get_acc_info(self) -> None:
-
+        """
+        Get account information 
+        """
         if mt5.account_info() is None:
             print("Account info is None!")
         else:
@@ -51,6 +59,8 @@ class MT5Class:
     def get_all_orders(self) -> None:
         """
         This function get the number of current active orders
+        Returns:
+            _type_: _description_ - All orders in place
         """
         orders = mt5.orders_total()
         
@@ -64,6 +74,11 @@ class MT5Class:
     def get_orders_total_by_symbol(self, symbol: str="USDJPY") -> int:
         """
         This function get the number of current active orders filter for a given symbol
+        Args:
+            symbol (str, optional): _description_. Defaults to "USDJPY". Symbol name
+
+        Returns:
+            int: _description_ - All order for a given symbol
         """
         orders = mt5.orders_get(symbol=symbol)
         
@@ -77,15 +92,116 @@ class MT5Class:
     def get_symbol_info(self, symbol: str="USDJPY"):
         """
         This function gets information for a given symbol
+        Args:
+            symbol (str, optional): _description_. Defaults to "USDJPY". - Symbol name
+
+        Returns:
+            _type_: _description_ - Information for a Given symbol
         """
+
         symbol_info = mt5.symbol_info(symbol)
         
         if symbol_info is None:
             print(f"{symbol} not found")
+            return None
         if not symbol_info.visible:
             print(f"{symbol} is not visible")
-        else:
-            print(symbol)
+            return None
+
+        return symbol_info
+    
+    def buy_symbol(self, price: float, lot: float, point: float, deviation: int):
+        """
+        The code block you provided is sending a trading request to buy a symbol at a
+        specific price.
+
+        Args:
+            price (float): _description_ - price to buy at
+            lot (float): _description_ - amount (lot) to buy
+            point (float): _description_ - 
+            deviation (int): _description_ - 
+
+        Returns:
+            _type_: _description_ - result of the buy request
+        """
+        request = {
+            "action": mt5.TRADE_ACTION_DEAL,
+            "symbol": symbol,
+            "volume": lot,
+            "type": mt5.ORDER_TYPE_BUY,
+            "price": price,
+            "sl": price - 100 * point,
+            "tp": price + 100 * point,
+            "deviation": deviation,
+            "magic": 234000,
+            "comment": "python script open",
+            "type_time": mt5.ORDER_TIME_GTC,
+            "type_filling": mt5.ORDER_FILLING_RETURN,
+        }
+                
+        # send a trading request
+        result = mt5.order_send(request)
+        
+        # check the execution result
+        print(f"1. order_send(): by {symbol} {lot} lots at {price} with deviation={deviation} points")
+        
+        return result
+    
+    
+    def sell_symbol(self, price: float, lot: float, point: float, deviation: int):
+        """
+        Sending a buy signal at a given price. 
+
+        Args:
+            price (float): _description_ - price to buy at
+            lot (float): _description_ - amount (lot) to buy
+            point (float): _description_ - 
+            deviation (int): _description_ - 
+
+        Returns:
+            _type_: _description_ - result of the buy request
+        """
+        request = {
+            "action": mt5.TRADE_ACTION_DEAL,
+            "symbol": symbol,
+            "volume": lot,
+            "type": mt5.ORDER_TYPE_BUY,
+            "price": price,
+            "sl": price - 100 * point,
+            "tp": price + 100 * point,
+            "deviation": deviation,
+            "magic": 234000,
+            "comment": "python script open",
+            "type_time": mt5.ORDER_TIME_GTC,
+            "type_filling": mt5.ORDER_FILLING_RETURN,
+        }
+                
+        # send a trading request
+        result = mt5.order_send(request)
+        
+        # check the execution result
+        print(f"1. order_send(): by {symbol} {lot} lots at {price} with deviation={deviation} points")
+        
+        return result
+    
+    def result_check(self, result):
+        """
+        Return the request result
+        Args:
+            result (_type_): _description_ - result for the request sent
+        """
+        if result.retcode != mt5.TRADE_RETCODE_DONE:
+            print(f"2. order_send failed, retcode={result.retcode}")
+            # request the result as a dictionary and display it element by element
+            result_dict=result._asdict()
+            for field in result_dict.keys():
+                print(f"  {field}={result_dict[field]}")
+                # if this is a trading request structure, display it element by element as well
+                if field=="request":
+                    traderequest_dict=result_dict[field]._asdict()
+                    for tradereq_filed in traderequest_dict:
+                        print(f" traderequest: {tradereq_filed}={traderequest_dict[tradereq_filed]}")
+
 
 
 def get_historic_data(fx_symbol, fx_timeframe, fx_count):
@@ -105,14 +221,29 @@ if __name__ == "__main__":
     mt5_obj.get_acc_info()
 
     # timeframe objects https://www.mql5.com/en/docs/python_metatrader5/mt5copyratesfrom_py
-    timeframe = mt5.TIMEFRAME_D1
+    timeframe = mt5.TIMEFRAME_M1
     symbol = 'USDJPY'
     # set time zone to UTC
     timezone = pytz.timezone("Etc/UTC")
     # create 'datetime' objects in UTC time zone to avoid the implementation of a local time zone offset
     utc_from = datetime(2010, 1, 10, tzinfo=timezone)
     utc_to = datetime(2020, 1, 11, tzinfo=timezone)
-    count = 13500
+    
+    """
+    time stamp :max count -- last timestamp: 
+    1 Day: 13675 -- 1971-01-04
+    12 hr: 20183 -- 1971-01-04 00:00:00
+    8 hr: 26678 -- 1971-01-04 00:00:00
+    4 hr: 46170 -- 1971-01-04 00:00:00
+    1 hr: 99999 -- 2007-11-29 11:00:00
+    30 mins: 99999 -- 2016-01-08 12:30:00
+    15 mins: 99999 -- 2020-01-15 23:00:00
+    5 mins: 99999 -- 2022-09-19 08:55:00
+    1 mins: 99999 -- 2023-10-13 08:56:00
+    """
+
+    count = 99999
+
 
     df = get_historic_data(symbol, timeframe, count)
 
